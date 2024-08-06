ba6f5a0545ea1decfae316dd291864e94ba4ae65
Saketh Devulapalli <162797972+sakethdss@users.noreply.github.com>
2024-08-06 10:58:41 +0530

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const response = await fetch('/send-email', {
+            const response = await fetch('/.netlify/functions/sendEmail', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
