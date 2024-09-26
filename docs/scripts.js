3ec713612a8adf1820aacccc8f40c6a8f3266fbf
Saketh Devulapalli <162797972+sakethdss@users.noreply.github.com>
2024-09-26 13:09:33 +0530

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -2,5 +2,7 @@ function toggleDetails(id) {
     const element = document.getElementById(id);
     if (element.classList.contains('hidden')) {
         element.classList.remove('hidden');
-    } 
+    } else {
+        element.classList.add('hidden');
+    }
 }
