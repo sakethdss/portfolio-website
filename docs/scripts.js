fe03cb5e5644ef440254dd04f8ada6d272188d56
Saketh Devulapalli <162797972+sakethdss@users.noreply.github.com>
2024-09-26 13:28:12 +0530

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,29 +1,20 @@
-function showDetails(id) {
-    let content = '';
+function readMore(id) {
+    let fullText = '';
     
     if (id === 'caring') {
-        content = `
-            <ul>
-                <li>Progressing towards a model to classify over 17,000 student essays using advanced NLP and ML techniques.</li>
-                <li>Evaluated unsupervised ML algorithms for pattern recognition and data analysis.</li>
-                <li>Devised methods to assess essays based on a rubric with 6 criteria.</li>
-                <li>Optimized data analysis and pre-processing with Python libraries, reducing redundancies by 30%.</li>
-            </ul>`;
+        fullText = `
+            Progressing towards a model to classify over 17,000 student essays using advanced NLP and ML techniques. Evaluated unsupervised ML algorithms for pattern recognition and data analysis. Devised methods to assess essays based on a rubric with 6 criteria. Optimized data analysis and pre-processing with Python libraries, reducing redundancies by 30%.
+        `;
     } else if (id === 'cnde') {
-        content = `
-            <ul>
-                <li>Architected 100+ web pages and revamped 3 existing dashboards to enhance user experience.</li>
-                <li>Reengineered web designs into mobile layouts for the iGenie mobile application.</li>
-                <li>Converted design prototypes into fully functional web pages using React.</li>
-            </ul>`;
+        fullText = `
+            Architected 100+ web pages and revamped 3 existing dashboards to enhance user experience. Reengineered web designs into mobile layouts for the iGenie mobile application. Converted design prototypes into fully functional web pages using React. Diagnosed 4 platforms to identify issues and improved user satisfaction by 40%.
+        `;
     } else if (id === 'tmss') {
-        content = `
-            <ul>
-                <li>Augmented 3 applications using Python (Tkinter) and Java.</li>
-                <li>Conducted web scraping to extract data from 5 websites.</li>
-                <li>Delivered 2 lead-gathering platforms within a 3-week timeframe.</li>
-            </ul>`;
+        fullText = `
+            Augmented 3 applications using Python (Tkinter) and Java. Conducted web scraping to extract data from 5 websites. Delivered 2 lead-gathering platforms within a 3-week timeframe. Collaborated with clients to tailor websites based on specific requirements.
+        `;
     }
 
-    document.getElementById(id).innerHTML = content;
+    document.getElementById(id + '-description').innerHTML = fullText;
+    document.querySelector(`button[onclick="readMore('${id}')"]`).style.display = 'none';
 }
